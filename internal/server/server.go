5cd913a577d02b6bceaa84b0e54acfd50234d631
Prathamesh Jadhav <prathameshj610@gmail.com>
2024-02-21 16:09:14 +0530

    Added echo server and postgres db

---
+++ internal/server/server.go
@@ -0,0 +1,59 @@
+package server
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/prathameshj610/fampay-youtube-assignment/internal/database"
+	"github.com/prathameshj610/fampay-youtube-assignment/internal/models"
+)
+
+type Server interface {
+	Start() error
+	Readiness(ctx echo.Context) error
+	Liveness(ctx echo.Context) error
+}
+
+type EchoServer struct {
+	echo *echo.Echo
+	DB   database.DatabaseClient
+}
+
+func NewEchoServer(db database.DatabaseClient) Server {
+	server := &EchoServer{
+		echo: echo.New(),
+		DB:   db,
+	}
+
+	server.registerRoutes()
+	return server
+}
+
+func (s *EchoServer) Start() error {
+
+	if err := s.echo.Start(":8080"); err != nil && err != http.ErrServerClosed{
+		log.Fatalf("Server shut down unexpectedly with error %s", err)
+		return err
+	}
+	return nil
+}
+
+func (s *EchoServer) registerRoutes() {
+	s.echo.GET("/readiness", s.Readiness)
+	s.echo.GET("/liveness", s.Liveness)
+
+}
+
+func (s *EchoServer) Readiness(ctx echo.Context) error {
+	ready := s.DB.Ready()
+	if ready {
+		return ctx.JSON(http.StatusOK, models.Health{Status: "OK"})
+	}
+
+	return ctx.JSON(http.StatusInternalServerError, models.Health{Status: "Failure"})
+}
+
+func (s *EchoServer) Liveness(ctx echo.Context) error {
+	return ctx.JSON(http.StatusOK, models.Health{Status: "OK"})
+}
--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"log"
+
+	"github.com/prathameshj610/fampay-youtube-assignment/internal/database"
+	"github.com/prathameshj610/fampay-youtube-assignment/internal/server"
+)
+
 func main() {
+
+	db, err := database.NewDatabaseClient()
+
+	if err != nil {
+		log.Fatalf("failed to initialize db client: %s", err)
+	}
+
+	srv := server.NewEchoServer(db)
+
+	if err := srv.Start(); err != nil {
+		log.Fatal(err.Error())
+	}
 
 }
